ec901b2a093fabdbe136a926b2bf82727abd1800
kkarczewski <serwisyrejestracje@wp.pl>
2014-04-25 00:19:26 +0200

    Naprawiony błąd z OpenMP
    Naprawiony błąd z OpenMP

--- macierz.c
+++ macierz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> //z flagą -lrt
-#include <omp.h>
-//KOMPILACJA gcc macierz.c -lrt
+#include <omp.h> // z flagą -fopenmp
+//KOMPILACJA gcc macierz.c -fopenmp -lrt
 int main(int argc, char *argv[]){
 	int j,l;//zmienne pomocnicze
 	int sizeA=atoi(argv[1]);//szerokość macierzy
@@ -23,19 +23,11 @@ int main(int argc, char *argv[]){
 	if((plik=fopen("dane.txt","r"))==0){
 		printf("Nie mogę otworzyć pliku\n");
 	}
-	//printf("Otworzyłem plik\n");
 //rezerwacja pamięci na macierz
 	A = (int*)malloc(sizeA*sizeB*sizeof(int*));	
-	//printf("Zająłem miejsce dla A\n");
 	while(fscanf(plik,"%d",&A[i])!=EOF){
 		i++;
 	}
-//Wydruk testowy A
-	//for(i=0;i<sizeA;i++){
-	//	for(j=0;j<sizeB;j++){
-	//		printf("A[%d,%d]=%d, ",i,j,A[i+j]);
-	//	}
-	//}
 //WEKTOR
 	int *B, *C;//wektor i wynik
 	if((plik=fopen("wektor.txt","r"))==0){
@@ -52,23 +44,19 @@ int main(int argc, char *argv[]){
 //KONIEC POMIARU IO
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	timeI=timespecDiff(&end, &start);
-	//printf("Czas wczytania plików=%2.5f\n",timeI);
-//Wydruk testowy B
-	//for(i=0;i<temp;i++){
-	//	printf("%d ",B[i]);
-	//}
 //mnożenie c[0]=a[0][0]*b[0]+a[0][1]*b[1]+...+a[0][j]*b[j]+...+a[0][n]*b[n]
 //		   c[i]=a[i][0]*b[0]+a[i][1]*b[1]+...+a[i][j]*b[j]+...+a[0][n]*b[n]
 //		   c[n]=a[n][0]*b[0]+a[n][1]*b[1]+...+a[n][j]*b[j]+...+a[n][n]*b[n]
-//n=size
 //POCZĄTEK POMIARU DLA OBLICZEŃ;
 	clock_gettime(CLOCK_MONOTONIC, &start);
-
-#pragma omp parallel for private(i,j), reduction(+ : C[i])
+int temp=0;
+	//#pragma omp parallel for private(i,j), reduction(+ : temp)
+	#pragma omp parallel for default(shared) private(temp,i,j)  
 	for(i=0;i<sizeA;i++){
 		for(j=0;j<sizeB;j++){
-			C[i]=C[i]+A[i*sizeA+j]*B[i];
+			C[i]+=A[i*sizeA+j]*B[i];
 		}
+//		C[i]=temp;
 	}
 //KONIC POMIARU DLA OBLICZEŃ
 	clock_gettime(CLOCK_MONOTONIC, &end);
